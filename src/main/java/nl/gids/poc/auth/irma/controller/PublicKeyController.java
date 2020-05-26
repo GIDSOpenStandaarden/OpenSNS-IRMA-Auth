032cdfb0e6a2367aa83263908ddd38244f03ea91
Roland Groen <roland@edia.nl>
2020-05-26 12:41:54 +0200

    Added public key endpoint for convenience.

---
+++ src/main/java/nl/gids/poc/auth/irma/controller/PublicKeyController.java
@@ -0,0 +1,21 @@
+package nl.gids.poc.auth.irma.controller;
+
+import nl.gids.poc.auth.irma.configuration.ServerConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ */
+@Controller
+public class PublicKeyController {
+	@Autowired
+	private ServerConfiguration serverConfiguration;
+
+	@RequestMapping(value = "/public_key.txt", produces = MediaType.TEXT_PLAIN_VALUE)
+	public String public_key() {
+		return serverConfiguration.getJwtPublicKey();
+	}
+}
