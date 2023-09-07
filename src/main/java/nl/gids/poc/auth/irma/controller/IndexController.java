eba8317064ad2f178843c8770004bcf89ee0276c
Joris Scharp <joris@headease.nl>
2023-09-07 15:35:41 +0200

    GVI-6: Allowing users to set the `pbdf.sidn-pbdf.uniqueid.uniqueid` attribute. This is still an experimental attribute, and cannot be set through the Yivi app (yet). Moved the html file to a Thyemeleaf location in order to use values from the model. The server-side rendering logic needs to know whether to show the set attribute section.

--- src/main/java/nl/gids/poc/auth/irma/controller/IndexController.java
+++ src/main/java/nl/gids/poc/auth/irma/controller/IndexController.java
@@ -6,6 +6,7 @@ import nl.gids.poc.auth.irma.services.ValidationService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,13 +17,22 @@ import java.net.URISyntaxException;
  */
 @Controller
 public class IndexController {
+	private final ApplicationConfiguration applicationConfiguration;
+	private final ValidationService validationService;
+
 	@Autowired
-	ApplicationConfiguration applicationConfiguration;
-	@Autowired
-	ValidationService validationService;
+	public IndexController(ApplicationConfiguration applicationConfiguration, ValidationService validationService) {
+		this.applicationConfiguration = applicationConfiguration;
+		this.validationService = validationService;
+	}
 
 	@RequestMapping("/")
-	public String index(@RequestParam(value = "redirect_uri") String redirectUri, @RequestParam(value = "attribute", required = false) String attribute, HttpSession session) throws URISyntaxException {
+	public String index(
+			@RequestParam(value = "redirect_uri") String redirectUri,
+			@RequestParam(value = "attribute", required = false) String attribute,
+			Model model,
+			HttpSession session) throws URISyntaxException {
+
 		session.setAttribute("redirectUri", redirectUri);
 		if (StringUtils.isEmpty(attribute)) {
 			attribute = applicationConfiguration.getDefaultAttribute();
@@ -30,6 +40,13 @@ public class IndexController {
 		session.setAttribute("attribute", attribute);
 		validationService.validateAttribute(attribute);
 		validationService.validateRedirectUri(redirectUri);
-		return "redirect:yivi.html";
+
+		//TODO: This is temporary code, can be removed after SIDN adds this login into the Yivi app. Can be configured
+		// if this is a more regular use-case
+		if("pbdf.sidn-pbdf.uniqueid.uniqueid".equals(attribute)) {
+			model.addAttribute("registerUrl", "https://middleware.vkn.gidsopenstandaarden.org/irma_put.html");
+		}
+
+		return "yivi";
 	}
 }
